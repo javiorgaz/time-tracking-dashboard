723f81f1adc9d24adb3194234f4adfe2c9861922
javier <javiorgaz.dev@gmail.com>
2025-12-23 12:35:34 +0100

    proyect v1

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const btns = document.getElementById("btn-container").querySelectorAll(".btn");
+import {timeData} from './data.js'
+
+const btns = document.querySelectorAll(".btn");
 console.log(btns);
+
+const items = document.querySelectorAll(".main-grid > *:not(:first-child)");
 
 for(let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
@@ -10,5 +14,24 @@ for(let i = 0; i < btns.length; i++) {
     this.classList.add("active");
   });
 };
+
+
+btns.forEach(function(btn) {
+  btn.addEventListener("click",function() {
+    for(let i = 0; i < items.length; i++) {
+
+      const optionEl = items[i].querySelector("#option");
+      const currentEl = items[i].querySelector("#hours");
+      const lastEl = items[i].querySelector("#last-week");
+
+      optionEl.textContent = timeData[i].title;
+      currentEl.textContent = timeData[i].timeframes[btn.id].current + "h";
+      lastEl.textContent = timeData[i].timeframes[btn.id].previous + "hrs";
+    }
+  });
+});
+
+
+
 
 
